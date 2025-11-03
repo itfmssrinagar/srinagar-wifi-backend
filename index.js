25f3ecdf51b3b97ac17c409957ff1c7b38932d42
Manish Bourai <manish.bourai@completrix.com>
2025-11-03 12:51:12 +0530

    Initialize Express server and MongoDB config
    Added basic Express server setup in index.js with middleware for CORS, cookies, and environment variables. Introduced config/db.js for MongoDB connection using mongoose. Updated package.json and package-lock.json to include dependencies for authentication, database, environment management, and development tooling.

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+
+const PORT= process.env.PORT || 5000;
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
